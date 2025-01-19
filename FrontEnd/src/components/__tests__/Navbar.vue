ec7b3e7662345fc832d59734e4e425c18f0d65ed
Teo <ib00055@rvt.lv>
2025-01-19 23:10:55 +0200

    Created Navbar but not complete

---
+++ FrontEnd/src/components/__tests__/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+    <header :class="{'scrolled-nav': scrolledPosition}">
+        <nav>
+            <div class="branding">
+
+            </div>
+
+            <ul v-show="!mobile" class="Navigation">
+                <li><RouterLink class="link" :to="{name: ''}">Home</RouterLink></li>
+                <li><RouterLink class="link" to="">about</RouterLink></li>
+                <li><RouterLink class="link" to="">Contact</RouterLink></li>
+                <li><RouterLink class="link" to="">Login</RouterLink></li>
+                <li><RouterLink class="link" to="">Register</RouterLink></li>
+            </ul>
+
+            <div class="icon">
+                    <i class="" ></i>
+                </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+@import '@/assets/navbar.css';
+</style>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,7 +1,10 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+
+import 'primeicons/primeicons.css'
 
 import App from './App.vue'
 import router from './router'
